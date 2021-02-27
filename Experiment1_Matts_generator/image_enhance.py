aff86e57a4a3adaf4e6cc7d854176f95f88ab383
QuYue <quyue1541@mail.dlut.edu.cn>
2021-02-27 22:55:56 +0800

    2

--- Experiment1_Matts_generator/image_enhance.py
+++ Experiment1_Matts_generator/image_enhance.py
@@ -8,11 +8,13 @@ Introduction: Image enhancement
 '''
 
 #%%
+%matplotlib qt5
 import numpy as np
 import cv2
 import matplotlib
 import matplotlib.pyplot as plt
 import random
+from blocks import Parm, Text_Selector, MattsPage
 
 
 #%% 
@@ -27,7 +29,44 @@ class Image_Enhance():
         self.desktop = True # 是否有桌面
         self.desktop_path = './Resource/Pictures'
 
+        self.Rotate()
+    
+    def Rotate(self):
+        hight, width = self.m_page.image.shape[:2]#self.m_page.width, self.m_page.hight
+        self.angle = np.random.randint(-30, 30)
+  
+        hightNew = int(width * np.abs(np.sin(np.radians(self.angle))) + hight * np.abs(np.cos(np.radians(self.angle))))
+        widthNew = int(width * np.abs(np.cos(np.radians(self.angle))) + hight * np.abs(np.sin(np.radians(self.angle))))
+        matRotation = cv2.getRotationMatrix2D((width//2, hight//2), self.angle, 1)
+        matRotation[0,2] += (widthNew - width)//2
+        matRotation[1,2] += (hightNew - hight)//2
+        self.imgRotation = cv2.warpAffine(self.m_page.image, matRotation,(widthNew,hightNew), borderValue=(0,0,0))
+        
+        x = self.m_page.label[0]['corr'][0]
+        y = self.m_page.label[0]['corr'][1]
+        print(matRotation.shape)
+        self.Q = np.dot(matRotation,np.array([[x],[y],[1]]))
+        for b in self.m_page.blocks:
+            for block in b:
+                left_top = np.dot(MatRotation,np.array([left_top+[1], right_bottem+[1]]).T)
+
+
+
 #%%
 if __name__ == '__main__':
-    a = Image_Enhance()
+    parm = Parm()
+    text_selector = Text_Selector()
+    page1 = MattsPage(parm, text_selector)
+    new_page1 = Image_Enhance(page1)
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(new_page1.m_page.image)
+
+    x = [new_page1.m_page.label[0]['corr'][0], new_page1.m_page.label[1]['corr'][0]]
+    y = [new_page1.m_page.label[0]['corr'][1], new_page1.m_page.label[1]['corr'][1]]
+    plt.scatter(x, y)
+    plt.subplot(1,2,2)
+    plt.imshow(new_page1.imgRotation)
+    plt.scatter(new_page1.Q[0], new_page1.Q[1])
+    plt.show()
     
